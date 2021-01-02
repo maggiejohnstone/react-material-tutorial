ad2acf5f7ecde149cfcb82df939494be56a8bb04
maggiejohnstone <m.nichole.j@gmail.com>
2021-01-02 12:28:28 -0600

    add index.js to exercises folder

--- tutorial-frontend/src/Components/App.js
+++ tutorial-frontend/src/Components/App.js
@@ -1,10 +1,14 @@
 import React, {Component, Fragment} from 'react';
 import { Header, Footer } from './Layout';
+import Exercises from './Exercises';
 
 export default class extends Component {
   render() {
     return <Fragment>
       <Header />
+
+      <Exercises />
+
       <Footer />
     </Fragment>
   }
